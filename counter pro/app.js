0c7033e2840e82f5fd88b77e08392b9b4afc4ff2
KATHIRAVAN.D <kathiravan0719@gmail.com>
2025-07-17 14:08:59 +0530

    Add files via upload

---
+++ counter pro/app.js
@@ -0,0 +1,30 @@
+
+
+// selecting elements from document
+
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countlabel");
+
+
+// declare a value for default
+let count = 0;
+
+
+//  functions on buttons
+
+increaseBtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
+}
+
+decreaseBtn.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+}
+
+resetBtn.onclick = function(){
+    count = 0;
+    countLabel.textContent = count;
+}
